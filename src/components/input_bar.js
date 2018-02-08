ed5a69f580e4956f95614bf8d183305667457978
Gabriel Lu <gabrielulu@gmail.com>
2018-02-08 11:01:00 +0800

    reset first commit

---
+++ src/components/input_bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Inputbar extends React.Component {
+  render() {
+    return (
+      <form>
+        <input placeholder='Do my homework.' />
+        <button />
+      </form>
+    )
+  }
+}
+
+export default Inputbar;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Inputbar from './components/input_bar';
+
+const Container = () => {
+  return(
+    <div>
+      <h1> My Todo </h1>
+      <Inputbar />
+    </div>
+  )
+}
+
+ReactDOM.render(<Container />, document.getElementById('root'))
