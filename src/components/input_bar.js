9bc8ac9854eda541c7e8cd9208be72da297d81ec
Gabriel Lu <gabrielulu@gmail.com>
2018-02-08 14:24:54 +0800

    finished core functionality for todo list

--- src/components/input_bar.js
+++ src/components/input_bar.js
@@ -1,31 +1,53 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class Inputbar extends React.Component {
+const TaskList = (props) => {
+  const tasks = props.list
+  const ListItems = tasks.map((task) =>
+    <li key={tasks.indexOf(task)}> {task} </li>
+  )
+  return (
+    ListItems
+  )
+}
+
+class Inputbar extends Component {
   constructor(props) {
     super(props);
     this.state={
-      inputText: ''
+      inputText: '',
+      todoList: []
     }
     this.handleSubmit = this.handleSubmit.bind(this)
     this.handleChange = this.handleChange.bind(this)
   }
   handleSubmit(e) {
     e.preventDefault()
-    console.log(e)
+    console.log('pre add',this.state.todoList)
+    let newtodoList = [...this.state.todoList,this.state.inputText]
+    console.log('newtodoList', newtodoList)
+    this.setState({
+      inputText: '',
+      todoList: newtodoList
+    })
+    console.log('post add',this.state.todoList)
   }
   handleChange(e) {
     this.setState({
       inputText: e.target.value
     })
   }
+
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <input value={this.state.inputText} onChange={this.handleChange} placeholder='Do my homework.' />
-        <button />
-      </form>
+      <div>
+        <ul> <TaskList list={this.state.todoList}/> </ul>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.inputText} onChange={this.handleChange} placeholder='Do my homework.' />
+          <button />
+        </form>
+      </div>
     )
   }
 }
 
-export default Inputbar;
+export default Inputbar
