e51006d80ec11b635cd66d5b753ecb6e54c7bcc5
Gabriel Lu <gabrielulu@gmail.com>
2018-02-08 11:32:38 +0800

    updated inputbar to change with state

--- src/components/input_bar.js
+++ src/components/input_bar.js
@@ -1,10 +1,27 @@
 import React from 'react';
 
 class Inputbar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      inputText: ''
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(e)
+  }
+  handleChange(e) {
+    this.setState({
+      inputText: e.target.value
+    })
+  }
   render() {
     return (
-      <form>
-        <input placeholder='Do my homework.' />
+      <form onSubmit={this.handleSubmit}>
+        <input value={this.state.inputText} onChange={this.handleChange} placeholder='Do my homework.' />
         <button />
       </form>
     )
