974bd4c0ade19451731964ae161cd87bbf139463
Gabriel Lu <gabrielulu@gmail.com>
2018-02-09 19:13:12 +0800

    added filters for task statuses

--- src/components/input_bar.js
+++ src/components/input_bar.js
@@ -7,46 +7,99 @@ class Inputbar extends Component {
     super(props);
     this.state={
       inputText: '',
-      todoList: []
-    }
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleChange = this.handleChange.bind(this)
-    this.handleDelete = this.handleDelete.bind(this)
-  }
-  handleSubmit(e) {
-    e.preventDefault()
-    let newtodoList = [...this.state.todoList,this.state.inputText]
-    this.setState({
-      inputText: '',
-      todoList: newtodoList
-    })
+      All: [],
+      status: 'all'
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.showComplete = this.showComplete.bind(this);
+    this.showPending = this.showPending.bind(this);
+    this.showAll = this.showAll.bind(this);
   }
   handleChange(e) {
     this.setState({
       inputText: e.target.value
-    })
+    });
+  }
+  handleSubmit(e) {
+    e.preventDefault();
+    let newAll = [...this.state.All, {item:this.state.inputText,status:true}];
+    this.setState({
+      inputText: '',
+      All: newAll
+    });
   }
   handleDelete(e) {
-    const deleteIndex = e.target.id
-    let newtodoList = this.state.todoList.slice(0)
-    newtodoList.splice(deleteIndex, 1)
+    const deleteIndex = e.target.id;
+    let newAll = this.state.All.slice(0);
+    newAll.splice(deleteIndex, 1);
     this.setState({
-      todoList: newtodoList
+      All: newAll
+    });
+  }
+  handleCheck(e) {
+    let toggleIndex = e.target.id;
+    console.log(e.target)
+    let newAll = this.state.All.slice(0);
+    newAll[toggleIndex].status = !newAll[toggleIndex].status;
+    this.setState({
+      All: newAll
+    });
+  }
+  showPending() {
+    this.setState({
+      status: 'pending'
     })
   }
+  showComplete() {
+    this.setState({
+      status: 'complete'
+    })
+  }
+  showAll() {
+    this.setState({
+      status: 'all'
+    })
+  }
+
+  renderItems(status) {
+    switch (status) {
+      case 'all':
+          return this.state.All
+      case 'pending':
+          return this.state.All.filter(
+            (el) => (el.status)
+          )
+      case 'complete':
+          return this.state.All.filter(
+            (el) => (!el.status)
+          )
+      default: 'all'
+    }
+  }
+
   render() {
     return (
       <div>
-        <ul>
-          <TaskList list={this.state.todoList} handleDelete={this.handleDelete}/>
-        </ul>
         <form onSubmit={this.handleSubmit}>
           <input value={this.state.inputText} onChange={this.handleChange} placeholder='Do your homework.' />
           <button />
-        </form>
+        </form> 
+        <ul>
+          <TaskList
+            list={this.renderItems(this.state.status)}
+            handleDelete={this.handleDelete}
+            handleCheck={this.handleCheck}
+            showComplete={this.showComplete}
+            showPending={this.showPending}
+            showAll={this.showAll}
+          />
+        </ul>
       </div>
     )
   }
-}
+};
 
-export default Inputbar
+export default Inputbar;
--- src/components/task_list.js
+++ src/components/task_list.js
@@ -3,11 +3,15 @@ import React from 'react';
 const TaskList = (props) => {
   const tasks = props.list
   const ListItems = tasks.map((task) =>
-    <li key={tasks.indexOf(task)}> {task} <button id={tasks.indexOf(task)} onClick={props.handleDelete}> X </button></li>
+    <li key={tasks.indexOf(task)}> <input key={tasks.indexOf(task)} id={tasks.indexOf(task)} onClick={props.handleCheck} type="checkbox" /> {task.item} <button id={tasks.indexOf(task)} onClick={props.handleDelete}> X </button></li>
   )
-
   return (
-    ListItems
+    <div>
+    {ListItems}
+    <button onClick={props.showComplete}> Completed </button>
+    <button onClick={props.showAll}> All </button>
+    <button onClick={props.showPending}> Pending </button>
+    </div>
   )
 }
 
