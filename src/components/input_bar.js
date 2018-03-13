ce7fdb3aad43db52d4970f448afb7310b32e545b
Gabriel Lu <gabrielulu@gmail.com>
2018-03-13 11:15:44 +0800

    mild refactoring under task_list conditional render

--- src/components/input_bar.js
+++ src/components/input_bar.js
@@ -1,89 +1,89 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import TaskList from './task_list';
+import TaskList from './task_list'
 
 let selectedValue = ''
 
 class Inputbar extends Component {
-  constructor(props) {
-    super(props);
-    this.state={
+  constructor (props) {
+    super(props)
+    this.state = {
       inputText: '',
       All: [],
       status: 'all'
-    };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleDelete = this.handleDelete.bind(this);
-    this.handleCheck = this.handleCheck.bind(this);
-    this.showComplete = this.showComplete.bind(this);
-    this.showPending = this.showPending.bind(this);
-    this.showAll = this.showAll.bind(this);
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.handleDelete = this.handleDelete.bind(this)
+    this.handleCheck = this.handleCheck.bind(this)
+    this.showComplete = this.showComplete.bind(this)
+    this.showPending = this.showPending.bind(this)
+    this.showAll = this.showAll.bind(this)
   }
-  handleChange(e) {
+  handleChange (e) {
     this.setState({
       inputText: e.target.value
-    });
+    })
   }
-  handleSubmit(e) {
-    e.preventDefault();
-    let newAll = [...this.state.All, {item:this.state.inputText,status:true}];
+  handleSubmit (e) {
+    e.preventDefault()
+    let newAll = [...this.state.All, {item: this.state.inputText, status: true}]
     this.setState({
       inputText: '',
       All: newAll
-    });
+    })
   }
-  handleDelete(e) {
-    selectedValue = e.target.value;
-    let newAll = this.state.All.slice(0);
-    let selectedIndex = newAll.findIndex(this.findvalue);
-
-    newAll.splice(selectedIndex, 1);
-
-    this.setState({
-      All: newAll
-    });
-  }
-  findvalue(e) {
+  findvalue (e) {
     return e.item === selectedValue
   }
-  handleCheck(e) {
-    selectedValue = e.target.value;
-    let newAll = this.state.All.slice(0);
-    let selectedIndex = newAll.findIndex(this.findvalue);
+  handleDelete (e) {
+    selectedValue = e.target.value
+    let newAll = this.state.All.slice(0)
+    let selectedIndex = newAll.findIndex(this.findvalue)
 
-    newAll[selectedIndex].status = !newAll[selectedIndex].status;
+    newAll.splice(selectedIndex, 1)
 
     this.setState({
       All: newAll
-    });
+    })
   }
-  showPending() {
+  handleCheck (e) {
+    selectedValue = e.target.value
+    let newAll = this.state.All.slice(0)
+    let selectedIndex = newAll.findIndex(this.findvalue)
+
+    newAll[selectedIndex].status = !newAll[selectedIndex].status
+
+    this.setState({
+      All: newAll
+    })
+  }
+  showPending () {
     this.setState({
       status: 'pending'
     })
   }
-  showComplete() {
+  showComplete () {
     this.setState({
       status: 'complete'
     })
   }
-  showAll() {
+  showAll () {
     this.setState({
       status: 'all'
     })
   }
 
-  renderItems(status) {
+  renderItems (status) {
     switch (status) {
       case 'all':
-          return this.state.All
+        return this.state.All
       case 'pending':
-          return this.state.All.filter(
+        return this.state.All.filter(
             (el) => (el.status)
           )
       case 'complete':
-          return this.state.All.filter(
+        return this.state.All.filter(
             (el) => (!el.status)
           )
       default:
@@ -91,7 +91,7 @@ class Inputbar extends Component {
     }
   }
 
-  render() {
+  render () {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -111,6 +111,6 @@ class Inputbar extends Component {
       </div>
     )
   }
-};
+}
 
-export default Inputbar;
+export default Inputbar
--- src/components/task_list.js
+++ src/components/task_list.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import '../main.css';
+import React from 'react'
+import '../main.css'
 
 const TaskList = (props) => {
   const tasks = props.list
 
   const ListItems = tasks.map((task, index) =>
     <li key={index}>
-      { (task.status) &&
-        <div className="taskEntry">
-          <input id={index} onChange={props.handleCheck} type="checkbox" value={task.item}/>
+      { (task.status)
+        ? <div className='taskEntry'>
+          <input id={index} onChange={props.handleCheck} type='checkbox' value={task.item} />
           <span> {task.item} </span>
         </div>
-      }
-      { (!task.status) &&
-        <div className="taskEntry">
-          <input id={index} onChange={props.handleCheck} type="checkbox" value={task.item} checked/>
-          <span className="strikethrough"> {task.item} </span>
+        : <div className='taskEntry'>
+          <input id={index} onChange={props.handleCheck} type='checkbox' value={task.item} checked />
+          <span className='strikethrough'> {task.item} </span>
         </div>
       }
-      <button className="deleteTask" id={tasks.indexOf(task)} onClick={props.handleDelete} value={task.item}>
+      <button className='deleteTask' id={tasks.indexOf(task)} onClick={props.handleDelete} value={task.item}>
         X
       </button>
     </li>
